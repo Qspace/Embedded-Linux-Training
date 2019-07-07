c9b606a6551f4fe91fbed4d63a3986961c58aac0
QuyenNgo <qsbk07@gmail.com>
2019-07-07 11:06:57 +0700

    Add snake game

---
+++ Bai_5/Demo/4_snake/SnakeNunchuck/main.cpp
@@ -0,0 +1,15 @@
+#include "mysnake.h"
+#include "uarthost.h"
+#include "nuchukhost.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    UartHost::S_getInstance("/dev/ttyUSB0");
+    NunchuckHost::S_getInstance();
+    MySnake w;
+    w.show();
+
+    return a.exec();
+}
